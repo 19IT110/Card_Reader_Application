b5edd1c66b292d40c419323b144c83b473ddc442
19IT110 <68862319+19IT110@users.noreply.github.com>
2022-01-24 22:03:37 -0800

    Add files via upload

---
+++ src/main/java/com/example/cardreaderapplication/ContactsActivity.java
@@ -0,0 +1,13 @@
+package com.example.cardreaderapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
